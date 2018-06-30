c58330cfca7b25f9e51b67b66f96c53680397c38
Dave Poirier <dave@idfusion.com>
2018-06-30 09:41:49 -0400

    cleaned up swift code
    Signed-off-by: Dave Poirier <dave@idfusion.com>

--- Sources/Bitstream.swift
+++ Sources/Bitstream.swift
@@ -12,54 +12,61 @@ import Bitter
 public class Bitstream: NSObject {
 
     private var data: Data
-
     private var bitsLeftInReadBuffer: Int = 0
     private var readByteIndex: Int = 0
     private var readBuffer: UInt8 = 0
-
     private var writeBuffer: UInt8 = 0
     private var bitsLeftInWriteBuffer: Int = 8
 
-    public init(data: Data ) {
-        self.data = data
+    enum Mode {
+        case readOnly
+        case writeOnly
+    }
+    let mode: Mode
+
+    public override init() {
+        self.data = Data()
+        self.mode = .writeOnly
     }
 
-    public func getNextBit() -> UInt8! {
-        if bitsLeftInReadBuffer == 0 && readByteIndex < data.count {
+    public init(data: Data) {
+        self.data = data
+        self.mode = .readOnly
+    }
+
+    public func getNextBit() -> UInt8? {
+        guard mode == .readOnly else { return nil }
+        if bitsLeftInReadBuffer == 0, readByteIndex < data.count {
             readBuffer = data[readByteIndex]
             bitsLeftInReadBuffer = 8
             readByteIndex += 1
         }
-        if bitsLeftInReadBuffer > 0 {
-            let bit = readBuffer.b7
-            readBuffer = readBuffer << 1
-            bitsLeftInReadBuffer -= 1
-            return bit
-        }
-        return nil
+        guard bitsLeftInReadBuffer > 0 else { return nil }
+        let bit = readBuffer.b7
+        readBuffer = readBuffer << 1
+        bitsLeftInReadBuffer -= 1
+        return bit
     }
 
-    public func getNextBits(_ bitsToRead: UInt8 ) -> UInt64! {
-        if bitsToRead > 64 {
-            return nil
-        }
-
+    public func getNextBits(_ bitsToRead: UInt8 ) -> UInt64? {
+        guard
+            mode == .readOnly,
+            bitsToRead <= 64
+            else { return nil }
         var bitsLeft = bitsToRead
         var combinedValue: UInt64 = 0
         while bitsLeft > 0 {
-            let bit = getNextBit()
-            if bit == nil {
-                return nil
-            }
+            guard let bit = getNextBit() else { return nil }
             combinedValue = combinedValue << 1
-            combinedValue = combinedValue | bit!.toU64
+            combinedValue = combinedValue | bit.toU64
             bitsLeft -= 1
         }
         return combinedValue
     }
 
     public func setNextBit(_ bit: UInt8 ) {
-        if( bitsLeftInWriteBuffer == 0 ) {
+        guard mode == .writeOnly else { return }
+        if bitsLeftInWriteBuffer == 0 {
             data.append(writeBuffer)
             writeBuffer = 0
             bitsLeftInWriteBuffer = 8
@@ -69,10 +76,10 @@ public class Bitstream: NSObject {
     }
 
     public func setNextBits(_ bitsToWrite: UInt8, from bits: UInt64) {
-        if( bitsToWrite > 64 ) {
-            return
-        }
-
+        guard
+            mode == .writeOnly,
+            bitsToWrite <= 64
+            else { return }
         var shiftedBits = bits << (64 - bitsToWrite.toU64)
         var bitsLeftToWrite = bitsToWrite
         while bitsLeftToWrite > 0 {
@@ -84,8 +91,11 @@ public class Bitstream: NSObject {
     }
 
     public func finalizedData() -> Data {
+        guard
+            mode == .writeOnly
+            else { return data }
         setNextBits(UInt8(bitsLeftInWriteBuffer), from: 0)
-        if( bitsLeftInWriteBuffer != 8 ) {
+        if bitsLeftInWriteBuffer != 8 {
             data.append(writeBuffer)
         }
         let finalizedData = data
@@ -93,19 +103,9 @@ public class Bitstream: NSObject {
     }
 
     public func dump() {
-        var abort = false
         var bits = ""
-        while abort == false {
-            let bit = getNextBit()
-            if bit == nil {
-                abort = true
-            } else {
-                if bit! == 0 {
-                    bits += "0"
-                } else {
-                    bits += "1"
-                }
-            }
+        while let bit = getNextBit() {
+            bits += "\(bit)"
         }
         print(bits)
     }
